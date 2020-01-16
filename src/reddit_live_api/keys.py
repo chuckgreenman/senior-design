b98af550ee84138f8d79c2c05190d33ad0ff0299
Isaac Mathers <isaactm98@gmail.com>
2020-01-16 09:08:56 -0500

    keys.py got removed. Adding back in.

---
+++ src/reddit_live_api/keys.py
@@ -0,0 +1,14 @@
+# Enter your information here
+
+CLIENT_ID = 'insert_id_here'
+CLIENT_SECRET = 'insert_secret_here'
+USER_AGENT = 'Reddit Live API v1.0 (by /u/botManCloud)'
+
+def getID():
+    return (CLIENT_ID)
+
+def getSecret():
+    return (CLIENT_SECRET)
+
+def getAgent():
+    return (USER_AGENT)
