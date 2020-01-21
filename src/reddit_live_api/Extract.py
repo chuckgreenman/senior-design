b133b0a08e0ac4376af0b568153f299a2a55a35f
Isaac Mathers <isaactm98@gmail.com>
2020-01-21 08:40:12 -0500

    Added fixes to community graph creator and switched to using pygraphviz for visualization

--- src/reddit_live_api/Extract.py
+++ src/reddit_live_api/Extract.py
@@ -3,6 +3,7 @@ from Subreddit import Subreddit
 from Utils import SubmissionType, sort_dictionary
 
 from matplotlib import pyplot as plt
+from networkx.drawing.nx_agraph import graphviz_layout
 import prawcore.exceptions
 import networkx as nx
 
@@ -16,6 +17,8 @@ def get_users_top_ranked_subs_to_action(users, num_srs, function_to_run):
         k = 0
         try:
             for ts in function_to_run(u, True):
+                # Lowering to prevent issues with dict
+                ts = ts.lower()
                 if ts in sr_user_top_subs:
                     sr_user_top_subs[ts] += 1
                 else:
@@ -98,7 +101,10 @@ def submission_plot_users_top_upvoted_subreddits(sr, sub_type=SubmissionType.TOP
                           " Most Upvoted SubReddits by {0} Current Top Submission Submitters")
 
 
-def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_less_than=15):
+# TODO: Look into creating max level away from initial sub instead of max num nodes
+# TODO: Change node size/intensity of edge to help give user more info on num users.
+# TODO: Look into filtering particularly big subs (AskReddit)
+def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_less_than=15, max_num_nodes=25):
     # This will hold the subs and the number of users who have this sub as one of their top subs
     # Using dicts for quicker lookup
     srs_and_user_count_total = {}
@@ -116,12 +122,15 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
     while continue_creation is True:
         # Needs to be reset on a per sub explored basis:
         # https://www.datacamp.com/community/tutorials/social-network-analysis-python
+        # Lower to prevent issues with keys in dict
+        next_sub = next_sub.lower()
+        print("Processing sub: {0}".format(next_sub))
         subreddit = Subreddit(next_sub)
         users = subreddit.get_submission_authors(sub_type)
         users_top_srs = get_users_top_ranked_subs_to_action(users, num_srs, User.get_submission_subreddits)
 
         # Track explored sub
-        srs_explored[subreddit.subreddit_name] = True
+        srs_explored[next_sub] = True
 
         for top_sr in users_top_srs:
             if top_sr in srs_and_user_count_total:
@@ -129,7 +138,9 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
             else:
                 srs_and_user_count_total[top_sr] = users_top_srs[top_sr]
             # Check to see if this sub's community interacts with top_sr enough to get over filter for exploration
-            if users_top_srs[top_sr] >= filter_subs_less_than and top_sr not in srs_explored:
+            # Only add more nodes to explore if max num nodes has not yet been exceeded.
+            if users_top_srs[top_sr] >= filter_subs_less_than and top_sr not in srs_explored and \
+                    len(srs_to_explore.keys()) < max_num_nodes:
                 srs_to_explore[top_sr] = True
 
         # Add to graph object
@@ -150,13 +161,22 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
             continue_creation = False
 
     # Space out the graph to make it nice to view and draw it.
-    nx.spring_layout(sub_network)
-    nx.draw_networkx(sub_network)
+    #nx.spring_layout(sub_network, k=0.15, iterations=20)
+    #nx.circular_layout(sub_network)
+    #nx.draw_networkx(sub_network)
+
+    pos = graphviz_layout(sub_network, prog="twopi", args="")
+    plt.figure(figsize=(8, 8))
+    nx.draw(sub_network, pos, node_size=20, alpha=0.5, node_color="blue", with_labels=True)
+    plt.axis("equal")
+
     print(nx.info(sub_network))
+    plt.title("Community Expansion from r/{0}\nBased on Users Top SubReddit to Submit".format(sr))
     plt.show()
 
 
-create_sub_network(sr="politics", filter_subs_less_than=10)
+create_sub_network(sr="coffee", filter_subs_less_than=5, max_num_nodes=10)
+# TODO: Document graphviz dependency
 # subreddit_plot_current_top_users_top_subreddits_to_submit("news", SubmissionType.TOP, 10)
 
 # submission_plot_commenters_other_top_subreddits(sr="conservative", match_title="clever title")
