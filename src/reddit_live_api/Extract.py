88819468ae9bfe115ca5a4d315a024c678de8f8e
Isaac Mathers <isaactm98@gmail.com>
2020-01-21 11:46:21 -0500

    Refactored code and added support for creating community graph via submission comments

--- src/reddit_live_api/Extract.py
+++ src/reddit_live_api/Extract.py
@@ -5,7 +5,52 @@ from Utils import SubmissionType, sort_dictionary
 from matplotlib import pyplot as plt
 from networkx.drawing.nx_agraph import graphviz_layout
 import prawcore.exceptions
+from praw.models import MoreComments
 import networkx as nx
+
+
+# Graceful way to get submissions from a subreddit
+def acquire_submission(sr, post_num=0, sub_type=SubmissionType.TOP, match_title=""):
+    subreddit = Subreddit(sr)
+    # Acquire the post we want examined
+    if match_title == "":
+        try:
+            sub = subreddit.get_submissions(sub_type)[post_num]
+        except IndexError:
+            print("Post number index out of bounds.")
+    else:
+        found = False
+        for p in subreddit.get_submissions(sub_type):
+            if p.title.lower() == match_title.lower():
+                sub = p
+                found = True
+                break
+        if found is False:
+            print("Failed to find post {0} on r/{1}\nUsing top post instead.".format(match_title, sr))
+            sub = subreddit.get_submissions(sub_type)[0]
+    return sub
+
+
+# From a submission, parse all comments, and acquire the users in a dict who commented
+def acquire_users_who_commented(sub, num_receive_comments_requests=0):
+    # Look at top comments. Get list of num_comments.
+    sub.comment_sort = 'top'
+    # This replace more allows us to get more comments, but it requires too much time.
+    # Leaving here for now as we brainstorm what to do.
+    #sub.comments.replace_more(limit=num_receive_comments_requests)
+    comments = sub.comments
+
+    users = {}
+    for c in comments:
+        if isinstance(c, MoreComments):
+            continue
+        # Accounts for deleted comments by seeing if author name is None
+        if c.author is not None and c.author.name not in users and c:
+            # Let's track score. Maybe we can cluster with it?
+            users[c.author.name] = [c.score]
+
+
+    return users
 
 
 # Should only call this function if the User function supports the ranking
@@ -55,36 +100,9 @@ def subreddit_plot_current_top_users_top_subreddits_to_submit(sr, sub_type=Submi
 
 def submission_plot_commenters_other_top_subreddits(sr, sub_type=SubmissionType.TOP, post_num=0, num_srs=3,
                                                     filter_subs_less_than=2, match_title=""):
-    subreddit = Subreddit(sr)
+    sub = acquire_submission(sr, post_num, sub_type, match_title)
 
-    # Acquire the post we want examined
-    if match_title == "":
-        try:
-            sub = subreddit.get_submissions(sub_type)[post_num]
-        except IndexError:
-            print("Post number index out of bounds.")
-    else:
-        found = False
-        for p in subreddit.get_submissions(sub_type):
-            if p.title.lower() == match_title.lower():
-                sub = p
-                found = True
-                break
-        if found is False:
-            print("Failed to find post {0} on r/{1}".format(match_title, sr))
-            return
-
-    # Look at top comments. Get list of all comments.
-    sub.comment_sort = 'top'
-    sub.comments.replace_more(limit=0)
-    comments = sub.comments.list()
-
-    users = {}
-    for c in comments:
-        # Accounts for deleted comments by seeing if author name is None
-        if c.author is not None and c.author.name not in users:
-            # Let's track score. Maybe we can cluster with it?
-            users[c.author.name] = [c.score]
+    users = acquire_users_who_commented(sub)
 
     filter_and_plot_users(get_users_top_ranked_subs_to_action(users.keys(), num_srs, User.get_comment_subreddits),
                           filter_subs_less_than,
@@ -104,7 +122,11 @@ def submission_plot_users_top_upvoted_subreddits(sr, sub_type=SubmissionType.TOP
 # TODO: Look into creating max level away from initial sub instead of max num nodes
 # TODO: Change node size/intensity of edge to help give user more info on num users.
 # TODO: Look into filtering particularly big subs (AskReddit)
-def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_less_than=15, max_num_nodes=25):
+# Creates a sub network of top subreddits. Can either be done by looking at users top subs to submit to or top subs to
+# comment on. If looking at top subs to comment on, this can take quite some time.
+def create_sub_network_from_top_srs(sr, sub_type=SubmissionType.TOP, num_srs=3,
+                                    filter_subs_less_than=15, max_num_nodes=25,
+                                    use_comments=False, post_num=0, match_title="", num_receive_comments_requests=5):
     # This will hold the subs and the number of users who have this sub as one of their top subs
     # Using dicts for quicker lookup
     srs_and_user_count_total = {}
@@ -125,9 +147,15 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
         # Lower to prevent issues with keys in dict
         next_sub = next_sub.lower()
         print("Processing sub: {0}".format(next_sub))
-        subreddit = Subreddit(next_sub)
-        users = subreddit.get_submission_authors(sub_type)
-        users_top_srs = get_users_top_ranked_subs_to_action(users, num_srs, User.get_submission_subreddits)
+
+        if use_comments is False:
+            subreddit = Subreddit(next_sub)
+            users = subreddit.get_submission_authors(sub_type)
+            users_top_srs = get_users_top_ranked_subs_to_action(users, num_srs, User.get_submission_subreddits)
+        else:
+            users = acquire_users_who_commented(acquire_submission(next_sub, post_num, sub_type, match_title),
+                                                num_receive_comments_requests)
+            users_top_srs = get_users_top_ranked_subs_to_action(users.keys(), num_srs, User.get_comment_subreddits)
 
         # Track explored sub
         srs_explored[next_sub] = True
@@ -147,7 +175,7 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
         for sub in users_top_srs:
             if users_top_srs[sub] >= filter_subs_less_than:
                 # Ensure all edges are created
-                sub_network.add_edge(subreddit.subreddit_name, sub)
+                sub_network.add_edge(next_sub, sub)
 
         # Reset
         next_sub = ""
@@ -161,21 +189,20 @@ def create_sub_network(sr, sub_type=SubmissionType.TOP, num_srs=3, filter_subs_l
             continue_creation = False
 
     # Space out the graph to make it nice to view and draw it.
-    #nx.spring_layout(sub_network, k=0.15, iterations=20)
-    #nx.circular_layout(sub_network)
-    #nx.draw_networkx(sub_network)
-
     pos = graphviz_layout(sub_network, prog="twopi", args="")
     plt.figure(figsize=(8, 8))
     nx.draw(sub_network, pos, node_size=20, alpha=0.5, node_color="blue", with_labels=True)
     plt.axis("equal")
 
     print(nx.info(sub_network))
-    plt.title("Community Expansion from r/{0}\nBased on Users Top SubReddit to Submit".format(sr))
+    if use_comments is False:
+        plt.title("Community Expansion from r/{0}\nBased on Users Top SubReddit to Submit".format(sr))
+    else:
+        plt.title("Community Expansion from r/{0}\nBased on Users Top SubReddit to Comment On".format(sr))
     plt.show()
 
 
-create_sub_network(sr="coffee", filter_subs_less_than=5, max_num_nodes=10)
+create_sub_network_from_top_srs(sr="cscareerquestions", filter_subs_less_than=5, max_num_nodes=10, use_comments=True)
 # TODO: Document graphviz dependency
 # subreddit_plot_current_top_users_top_subreddits_to_submit("news", SubmissionType.TOP, 10)
 
