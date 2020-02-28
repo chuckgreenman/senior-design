495e26728bd801d8888acc851e8e517bf699fdb4
Chuck Greenman <chuck@chucks-mac-mini.home>
2020-02-27 21:29:26 -0500

    Add pages and routers into the Vue app.

--- src/oscar-vue/src/App.vue
+++ src/oscar-vue/src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="menu">
+      <img id="logo" src="./assets/oscar_logo.png" alt="Oscar logo"> 
+      <div class="items">
+        <div class="item"><router-link to="/">Home</router-link></div>
+        <div class="item"><router-link to="/about">About</router-link></div>
+      </div>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
@@ -21,8 +20,26 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  display: flex;
+  justify-content: space-between;
+}
+
+.items {
+  display: flex;
+}
+
+.item {
+  margin-left: 10px;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#logo {
+  max-height: 50px;
 }
 </style>
