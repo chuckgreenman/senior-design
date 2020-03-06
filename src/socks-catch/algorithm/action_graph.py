c9161bfb019cda0786cd1cb1f27d1eb4ca109a44
Chuck Greenman <chuck@perceptigen.com>
2020-03-06 09:51:09 -0500

    Stub the action graph.

---
+++ src/socks-catch/algorithm/action_graph.py
@@ -0,0 +1,3 @@
+class ActionGraph():
+  def __init__(self, user_id):
+    print(user_id)
--- src/socks-catch/cli.py
+++ src/socks-catch/cli.py
@@ -1,5 +1,6 @@
 import click
 from utilities.db_setup import DbSetup
+from algorithm.action_graph import ActionGraph
 
 @click.group()
 def cli():
@@ -12,7 +13,13 @@ def cli():
 def dbsetup(env):
   DbSetup(env)
 
+@click.command()
+@click.argument('user_id')
+def actiongraph(user_id):
+  ActionGraph(user_id)
+
 cli.add_command(dbsetup)
+cli.add_command(actiongraph)
 
 if __name__ == "__main__":
   cli()
