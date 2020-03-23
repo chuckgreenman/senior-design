1c0c4dc68a8352a3043f7e81a5ebec8f10737a9b
Isaac Mathers <isaactm98@gmail.com>
2020-03-23 13:20:06 -0700

    Init commit messing around with Vue

--- src/oscar-vue/src/App.vue
+++ src/oscar-vue/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="OSCAR logo" src="./assets/oscar_logo.png">    
+    <OscarHeader/>
+    <MainTable/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OscarHeader from './components/Header.vue'
+import MainTable from './components/MainTable.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {    
+    OscarHeader,
+    MainTable
   }
 }
 </script>
---
+++ src/oscar-vue/src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Welcome to OSCAR!</h1>    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'OscarHeader'  
+}
+</script>
