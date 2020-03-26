7cc959317ea474f44d77661ad259cac730184958
Isaac Mathers <isaactm98@gmail.com>
2020-03-26 14:56:25 -0700

    Adds initial attempt at http requests

--- src/vuetify-demo/src/components/MainTabs.vue
+++ src/vuetify-demo/src/components/MainTabs.vue
@@ -23,11 +23,12 @@
                     <v-col>
                     <v-text-field
                         label="User"
+                        v-model="user"
                         single-line
                     ></v-text-field>
                     </v-col>
                     <v-col>
-                    <v-btn>Execute</v-btn>
+                    <v-btn v-on:click="getUser">Execute</v-btn>
                     </v-col>
                 </v-row>
                 </v-container>
@@ -40,6 +41,7 @@
                     <v-col>
                     <v-text-field
                         label="SubReddit"
+                        v-model="subreddit"
                         single-line
                     ></v-text-field>
                     </v-col>
@@ -53,3 +55,27 @@
     </v-tabs>
   </v-card>
 </template>
+
+<script>
+export default {
+  name: 'MainTabs',
+  methods: {
+      // Will get user in user textfield and perform
+      // http post request to flask server at 127.0.0.1
+      // TODO: Check for blank user name
+    getUser: function () {
+      var req = '127.0.0.1/user?name=' + this.user
+      console.log(req)
+    //   this.$http.get(req).then(response => {
+ 
+    // // get body data
+    // this.someData = response.body;
+    // console.log(this.someData)
+    // }, response => {
+    // // error callback
+    // console.log('Error: ' + response.body)
+    // });
+    }
+  }  
+}
+</script>
