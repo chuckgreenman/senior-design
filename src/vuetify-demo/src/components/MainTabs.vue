d8209ac268cb9652d052e6be3cc81e3cd8420c0d
Isaac Mathers <isaactm98@gmail.com>
2020-03-27 11:02:41 -0700

    Switches html requests over to axios

--- src/vuetify-demo/src/components/MainTabs.vue
+++ src/vuetify-demo/src/components/MainTabs.vue
@@ -46,7 +46,7 @@
                     ></v-text-field>
                     </v-col>
                     <v-col>
-                    <v-btn>Execute</v-btn>
+                    <v-btn v-on:click="getSubReddit">Execute</v-btn>
                     </v-col>
                 </v-row>
                 </v-container>
@@ -57,23 +57,53 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'MainTabs',
   methods: {
       // Will get user in user textfield and perform
-      // http post request to flask server at 127.0.0.1
+      // http get request to flask server at 127.0.0.1
       // TODO: Check for blank user name
-    getUser: function () {
-      var req = '127.0.0.1/user?name=' + this.user
-      console.log(req)
-    //   this.$http.get(req).then(response => {
+    getUser: function () {      
+      console.log("In getUser2") 
+      
+      axios.get('http://localhost:5000/user', { headers: {
+	  'Access-Control-Allow-Origin': '*',
+	}, params: { name: this.user }}, {timeout: 0})
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });            
+    //   this.$http.get('http://localhost:5000/user', {params: {name: this.user}}).then(response => {
  
-    // // get body data
-    // this.someData = response.body;
-    // console.log(this.someData)
-    // }, response => {
-    // // error callback
-    // console.log('Error: ' + response.body)
+    //     // get body data
+    //     this.someData = response.body;
+    //     console.log(this.someData)
+    //     }, response => {
+    //     // error callback
+    //     console.log('Error: ' + response.body)
+    // });
+    },
+       // Will get subreddit in subreddit textfield and perform
+      // http get request to flask server at 127.0.0.1
+      // TODO: Check for blank user name
+    getSubReddit: function () {    
+      console.log("In getsubreddit")  
+      axios.get('http://localhost:5000/subreddit', { params: { name: this.subreddit }})
+      .then((response) => {
+        console.log(response);
+      }, (error) => {
+        console.log(error);
+      });             
+    //   this.$http.get('http://localhost:5000/subreddit', {params: {name: this.subreddit}}).then(response => {
+ 
+    //     // get body data
+    //     this.someData = response.body;
+    //     console.log(this.someData)
+    //     }, response => {
+    //     // error callback
+    //     console.log('Error: ' + response.body)
     // });
     }
   }  
