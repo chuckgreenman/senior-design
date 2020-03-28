9fc10d1924fd2778abe97041abf20eaa834c9a1b
Chuck Greenman <chuck@perceptigen.com>
2020-03-28 11:06:49 -0400

    Adding the dataloader code.

--- src/socks-catch/utilities/dataloader.py
+++ src/socks-catch/utilities/dataloader.py
@@ -1,6 +1,6 @@
 import os
 import binascii
-import zipfile
+import gzip
 
 from models.activity import Activity
 from utilities.db_interact import DbInteract
@@ -18,8 +18,9 @@ class DataLoader:
 
   def verify_dir_structure(self, path):
     for file in os.listdir(path):
-      if not self.is_gz_file(os.path.join(path, file)):
-        raise Exception("Invalid file type detected", file)
+      if not os.path.isdir(os.path.join(path, file)):
+        if not self.is_gz_file(os.path.join(path, file)):
+          raise Exception("Invalid file type detected", file)
 
   def verify_activity_type(self, act_type):
     if act_type in activity_types:
@@ -29,7 +30,7 @@ class DataLoader:
 
   def is_gz_file(self, file):
     with open(file, 'rb') as test_f:
-      return os.path.isdir(file) or binascii.hexlify(test_f.read(2)) == b'1f8b'
+      return binascii.hexlify(test_f.read(2)) == b'1f8b'
 
   def load_activity(self, path, act_type):
     files_to_import = os.listdir(path)
@@ -39,12 +40,32 @@ class DataLoader:
       print("Importing file", file_num+1, "of", file_count, file_name)
       self.import_file(path, file_name, act_type)
 
+  def index_by_field_name(self, needle, haystack):
+    for i, element in enumerate(haystack):
+      if element == needle:
+        return i
+    return None
+
   def import_file(self, path, file_name, act_type):
-    os.mkdir(os.path.join(path, "working_directory"))
+    field_list = None
+    with gzip.open(os.path.join(path, file_name), 'rb') as activity_file:
+      for line_number, line in enumerate(activity_file):
+        if line_number == 0:
+          field_list = line.decode('utf-8').split(",")
 
-    with zipfile.ZipFile(os.path.join(path, file_name)) as zip_ref:
-      zip_ref.extractall(os.path.join(path, "working_directory", file_name))
+        line = line.decode('utf-8').split(",")
+        if len(field_list) != len(line):
+          continue
 
-    with open(os.join.path(path, "working_directory", file_name)) as activity_file:
-      for line_number, line in activity_file:
-        print(line_number, line)
+        if act_type == 'comment':
+          subreddit_id_index = self.index_by_field_name("subreddit_id", field_list)
+          user_id_index = self.index_by_field_name("author", field_list)
+          created_utc_index = self.index_by_field_name("created_utc", field_list)
+          
+          a = Activity(line[subreddit_id_index], line[user_id_index], 'comment', line[created_utc_index])
+        else:
+          subreddit_id_index = index_by_field_name("subreddit_id", field_list)
+          user_id_index = index_by_field_name("author", field_list)
+          created_utc_index = index_by_field_name("created_utc", field_list)
+          a = Activity(line[subreddit_id_index], line[user_id_index], 'link', line[created_utc_index])
+
