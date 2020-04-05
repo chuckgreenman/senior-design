deb8a082f15e7027fd69319c90c66ef18ebdf866
Isaac Mathers <isaactm98@gmail.com>
2020-04-05 14:40:02 -0700

    Adds user request graphs

--- src/vuetify-demo/src/components/MainTabs.vue
+++ src/vuetify-demo/src/components/MainTabs.vue
@@ -30,7 +30,46 @@
                   <v-flex md2>
                   <v-btn v-on:click="getUser">Execute</v-btn>
                   </v-flex>
-              </v-layout>                  
+              </v-layout>     
+
+                <div class="userResults">
+                <v-layout row>
+                    <v-flex md6>
+                    <h3> Most Popular Words for {{this.user}} </h3>
+                    </v-flex>
+                    <v-flex md6>
+                    <h3> Most Popular Links for {{this.user}} </h3>  
+                    </v-flex>
+                </v-layout>     
+
+                <v-layout row>
+                    <v-flex md6>
+                    <v-chart :options="popularWordsChart"></v-chart> 
+                    </v-flex>
+                    <v-flex md6>
+                    <v-chart :options="popularLinksChart"></v-chart>
+                    </v-flex>
+                </v-layout> 
+
+                <v-layout row>
+                    <v-flex md6>
+                    <h3> Proportion of Controversial Posts for {{this.user}} </h3> 
+                    </v-flex>     
+                    <v-flex md6>
+                    <h3> SubReddits that {{this.user}} Posts on </h3> 
+                    </v-flex>               
+                </v-layout>
+
+                <v-layout row>
+                    <v-flex md6>
+                    <v-chart :options="controversyChart"></v-chart> 
+                    </v-flex>      
+                    <v-flex md6>
+                    <v-chart :options="initSubRedditRelationChart"></v-chart> 
+                    </v-flex>                     
+                </v-layout> 
+                </div>    
+
                 </v-container>
             </v-card>
         </v-tab-item>
@@ -123,7 +162,231 @@ export default {
       axios.get('http://localhost:5000/user', {         
          params: { name: this.user }}, {timeout: 0})
       .then((response) => {
-        console.log(response);
+        // Reveals elements
+        var revealElements = document.getElementsByClassName("userResults");
+        for(var i = 0; i < revealElements.length; i++)
+        {            
+            revealElements.item(i).className = "showResults";
+        }
+       
+        // Create items array
+        var legend_data = Object.keys(response.data.popular_words).map(function(key) {
+          return [key, response.data.popular_words[key]];
+        });
+        
+        // Sort the array based on the second element
+        legend_data.sort(function(first, second) {
+          return second[1] - first[1];
+        });
+                
+        // Create a new array with only the first 16 items taking only the keys
+        legend_data = legend_data.slice(0, 16);
+
+        for(i = 0; i < legend_data.length; i++){
+          legend_data[i] = legend_data[i][0]; // only store the key value
+        }        
+        
+        var series_data = [];
+        // Fill series_data in expected format
+        for (var key in response.data.popular_words){
+          series_data.push({value: response.data.popular_words[key], name: key});
+        }
+        // Build data for popular words chart
+        this.popularWordsChart = {      
+            tooltip: {
+                trigger: 'item',
+                formatter: '{a} <br/>{b}: {c} ({d}%)'
+            },
+            legend: {
+                orient: 'vertical',
+                left: 10,                
+                data: legend_data
+            },
+            series: [
+                {
+                    name: 'Most Popular Words from\n r/' + this.subreddit,
+                    type: 'pie',
+                    radius: ['50%', '70%'],
+                    avoidLabelOverlap: false,
+                    label: {
+                        show: false,
+                        position: 'center'
+                    },
+                    emphasis: {
+                        label: {
+                            show: true,
+                            fontSize: '30',
+                            fontWeight: 'bold'
+                        }
+                    },
+                    labelLine: {
+                        show: false
+                    },
+                    data: series_data
+                }
+            ]
+        }
+
+        // Create items array
+        legend_data = Object.keys(response.data.most_linked_websites).map(function(key) {
+          return [key, response.data.most_linked_websites[key]];
+        });
+
+        // Sort the array based on the second element
+        legend_data.sort(function(first, second) {
+          return second[1] - first[1];
+        });
+
+        // Create a new array with only the first 16 items taking only the keys
+        legend_data = legend_data.slice(0, 16);
+        
+        for(i = 0; i < legend_data.length; i++){
+          legend_data[i] = legend_data[i][0]; // only store the key value
+        }
+
+        series_data = [];
+        // Fill series_data in expected format
+        for (key in response.data.most_linked_websites){
+          if (response.data.most_linked_websites[key] > 1){
+            series_data.push({value: response.data.most_linked_websites[key], name: key});
+          }          
+        }
+        // Build data for popular links chart
+        this.popularLinksChart = {          
+          tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b}: {c} ({d}%)'
+          },
+          legend: {
+              orient: 'vertical',
+              left: 10,              
+              data: legend_data
+          },
+          series: [
+              {
+                  name: 'Most Linked to Websites from\n r/' + this.subreddit,
+                  type: 'pie',
+                  radius: ['50%', '70%'],
+                  avoidLabelOverlap: false,
+                  label: {
+                      show: false,
+                      position: 'center'
+                  },
+                  emphasis: {
+                      label: {
+                          show: true,
+                          fontSize: '30',
+                          fontWeight: 'bold'
+                      }
+                  },
+                  labelLine: {
+                      show: false
+                  },
+                  data: series_data
+              }
+          ]
+      }
+
+      // Create items array
+        legend_data = Object.keys(response.data.proportion_controversial_posts).map(function(key) {
+          return [key, response.data.proportion_controversial_posts[key]];
+        });
+
+        // Sort the array based on the second element
+        legend_data.sort(function(first, second) {
+          return second[1] - first[1];
+        });
+
+        // Create a new array with only the first 16 items taking only the keys
+        legend_data = legend_data.slice(0, 16);
+        
+        for(i = 0; i < legend_data.length; i++){
+          legend_data[i] = legend_data[i][0]; // only store the key value
+        }
+
+        series_data = [];
+        // Fill series_data in expected format
+        for (key in response.data.proportion_controversial_posts){
+          series_data.push({value: response.data.proportion_controversial_posts[key], name: key});
+        }
+        // Build data for popular links chart
+        this.controversyChart = {          
+          tooltip: {
+              trigger: 'item',
+              formatter: '{a} <br/>{b}: {c} ({d}%)'
+          },
+          legend: {
+              orient: 'vertical',
+              left: 10,              
+              data: legend_data
+          },
+          series: [
+              {
+                  name: 'Proportion of Controversial Posts on\n r/' + this.subreddit,
+                  type: 'pie',
+                  radius: ['50%', '70%'],
+                  avoidLabelOverlap: false,
+                  label: {
+                      show: false,
+                      position: 'center'
+                  },
+                  emphasis: {
+                      label: {
+                          show: true,
+                          fontSize: '30',
+                          fontWeight: 'bold'
+                      }
+                  },
+                  labelLine: {
+                      show: false
+                  },
+                  data: series_data
+              }
+          ]
+        }
+
+        series_data = [['count', 'subreddit']];
+
+        var max_value = 0; // Used for visual map in initSubRedditRelationChart
+        // Fill series_data in expected format
+        for (key in response.data.top_subreddits){
+          series_data.push([response.data.top_subreddits[key], key]);
+          if (response.data.top_subreddits[key] > max_value){
+            max_value = response.data.top_subreddits[key];
+          }
+        }        
+
+        this.initSubRedditRelationChart = {
+            dataset: {
+                source: series_data
+            },
+            grid: {containLabel: true},
+            xAxis: {name: 'count'},
+            yAxis: {type: 'category'},
+            visualMap: {
+                orient: 'horizontal',
+                left: 'center',
+                min: 0,
+                max: max_value,
+                text: ['High Count', 'Low Count'],
+                // Map the score column to color
+                dimension: 0,
+                inRange: {
+                    color: ['#1145f0', 'rgb(255, 67, 1)'] //blue to orange
+                }
+            },
+            series: [
+                {
+                    type: 'bar',
+                    encode: {
+                        // Map the "count" column to X axis.
+                        x: 'count',
+                        // Map the "subreddit" column to Y axis
+                        y: 'subreddit'
+                    }
+                }
+            ]
+        };
       }, (error) => {
         console.log(error);
       });                
@@ -482,6 +745,10 @@ export default {
 }
 
 .subRedditResults{
+  display: none
+}
+
+.userResults{
   display: none
 }
 
